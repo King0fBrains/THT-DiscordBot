94d69c53ef19ee6f245af3ae6e8daf3a8325790b
jackd <jackdscarfo@gmail.com>
2023-11-16 15:24:00 -0500

    Update owner module

--- cogs/owner.py
+++ cogs/owner.py
@@ -1,6 +1,5 @@
 import discord
 from discord.ext import commands
-from discord.ext.commands import bot
 
 
 class Owner(commands.Cog):
@@ -8,16 +7,16 @@ class Owner(commands.Cog):
         self.bot = bot
 
     async def cog_check(self, ctx):
-        return True if ctx.author.id == 396492370998460420  else False
+        return commands.is_owner()
+
     @commands.Cog.listener()
-    async def on_ready(selfs):
+    async def on_ready(self):
         print('Owner Cog is ready.')
 
     @commands.command(name='shutdown', help='This command shuts down the bot.')
     async def shutdown(self, ctx):
         await ctx.send('Shutting down...')
         await ctx.bot.close()
-
 
 
 async def setup(bot):
