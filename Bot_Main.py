45b753d6a07dc59d6070f2181ce8d5dc0d265a3c
jackd <jackdscarfo@gmail.com>
2023-11-16 15:23:46 -0500

    Update remote control module

--- Bot_Main.py
+++ Bot_Main.py
@@ -19,11 +19,6 @@ async def load():
         print(f'Loaded {cog} cog.')
 
 
-@bot.command()
-async def hello(ctx):
-    await ctx.send('Hello!')
-
-
 @bot.event
 async def on_ready():
     await bot.change_presence(activity=discord.Game(name='with the API'))
@@ -34,6 +29,43 @@ async def on_ready():
 async def change(ctx):
     await bot.change_presence(activity=discord.Game(name='I have now changed my status!'))
     await ctx.send("I'm now playing with the API!")
+
+
+# class MyHelp(commands.HelpCommand):
+#
+#     async def send_bot_help(self, mapping):
+#         """
+#         This is triggered when !help is invoked.
+#
+#         This example demonstrates how to list the commands that the member invoking the help command can run.
+#         """
+#         filtered = await self.filter_commands(self.context.bot.commands, sort=True)  # returns a list of command objects
+#         names = [command.name for command in filtered]  # iterating through the commands objects getting names
+#         available_commands = "\n".join(names)  # joining the list of names by a new line
+#         embed = discord.Embed(description=available_commands)
+#         await self.context.send(embed=embed)
+#
+#     async def send_command_help(self, command):
+#         """This is triggered when !help <command> is invoked."""
+#         await self.context.send(command.help)
+#
+#     async def send_group_help(self, group):
+#         """This is triggered when !help <group> is invoked."""
+#         await self.context.send("This is the help page for a group command")
+#
+#     async def send_cog_help(self, cog):
+#         """This is triggered when !help <cog> is invoked."""
+#         commands_cog = cog.get_commands()
+#         embed = discord.Embed(title=cog.qualified_name)
+#         await self.context.send("This is the help page for a cog")
+#
+#     async def send_error_message(self, error):
+#         """If there is an error, send an embed containing the error."""
+#         channel = self.get_destination()  # this defaults to the command context channel
+#         await channel.send(error)
+#
+#
+# bot.help_command = MyHelp()
 
 
 async def main():
--- cogs/remote_control.py
+++ cogs/remote_control.py
@@ -1,30 +1,30 @@
-import discord
 from discord.ext import commands
 import pyautogui
 
 
-class Remote_Control(commands.Cog):
+class RemoteControl(commands.Cog):
+    Keys = ['z', 'x', 'up', 'down', 'left', 'right', 'enter', 'backspace', 'a', 's']
+    Roles = (878727109143580683, 810013892670521364, 993305944412921967, 1003816787181314109)
+
     def __init__(self, bot):
         self.bot = bot
-        self.status = 0
+        self.status = 2
+        self.state = False
 
-    Possible_Keys = ['a', 'b', 'up', 'down', 'left', 'right', 'start', 'select', 'l', 'r']
-    for item in Possible_Keys:
-        item.strip("'")
-
-    @commands.Cog.listener()
-    async def on_ready(self):
-        print('Game is ready.')
-
-    @commands.command(name='remote', help="This command enables and disables mass remote control.")
-    @commands.has_any_role(878727109143580683, 810013892670521364, 993305944412921967)
+    @commands.command(name='remote',
+                      help="This command allows for admins/moderators to manage the remote control state.",
+                      brief="This command enables and disables mass remote control.")
+    @commands.has_any_role(878727109143580683, 810013892670521364, 993305944412921967, 1003816787181314109)
     async def remote(self, ctx, arg):
         if arg == 'on':
-            self.status = 1
+            self.state = True
             await ctx.send('Remote control enabled.')
         elif arg == 'off':
-            self.status = 0
+            self.state = False
             await ctx.send('Remote control disabled.')
+        elif arg == 'all':
+            self.status = 1
+            await ctx.send('Remote control enabled for all users.')
         elif arg == 'admin':
             self.status = 2
             await ctx.send('Remote control enabled for admins.')
@@ -33,23 +33,35 @@ class Remote_Control(commands.Cog):
         else:
             await ctx.send('Invalid input. Please use either "on" or "off".')
 
-    @commands.command(name='press', help="This command clicks the requested button.")
-    async def press(self, ctx, arg):
-        if self.status != 0:
-            if self.status == 2:
-                if 878727109143580683 or 810013892670521364 or 993305944412921967 in [role.id for role in ctx.author.roles]:
-                    if arg in self.Possible_Keys:
-                        pyautogui.press(arg, interval=0.5)
-                        await ctx.send(f'Pressed the requested button: {arg}')
-                    else:
-                        await ctx.send(
-                            f"Invalid input. Please use one of the following: {'%s' % ', '.join(map(str, self.Possible_Keys))}.")
-            elif arg in self.Possible_Keys:
-                pyautogui.press(arg, interval=0.5)
-                await ctx.send(f'Pressed the requested button: {arg}')
+    @commands.command(name='press', brief="This command clicks the requested button.")
+    async def press(self, ctx, key):
+        status = self.status
+        state = self.state
+        keys = self.Keys
+        roles = self.Roles
+
+        if state is False:
+            await ctx.send(f"Remote state is not on")
+        elif status == 1:
+            if key in keys:
+                pyautogui.press(key, interval=0.5)
+                await ctx.send(f'Pressed the requested button: {key}')
             else:
-                await ctx.send(f"Invalid input. Please use one of the following: {'%s' % ', '.join(map(str, self.Possible_Keys))}.")
+                await ctx.send(
+                    f"Invalid input. Please use one of the following: {'%s' % ', '.join(map(str, keys))}.")
+        else:
+            if commands.has_any_role(878727109143580683, 810013892670521364, 993305944412921967,
+                                     1003816787181314109) and key in keys:
+                pyautogui.press(key, interval=0.5)
+                await ctx.send(f'Pressed the requested button: {key}')
+            elif not commands.has_any_role(878727109143580683, 810013892670521364, 993305944412921967,
+                                           1003816787181314109):
+                await ctx.send(f"You do not have permission to use this command.")
+
+            else:
+                await ctx.send(
+                    f"Invalid input. Please use one of the following: {'%s' % ', '.join(map(str, keys))}.")
 
 
 async def setup(bot):
-    await bot.add_cog(Remote_Control(bot))
+    await bot.add_cog(RemoteControl(bot))
